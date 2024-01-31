067e6828ba6c158a6085fd5300e220476fc8a60d
Pcosby5 <pcosby50@gmail.com>
2024-01-31 21:04:48 +0100

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
 # A Bash script that sets up web servers for the deployment of web_static
+
+# Install Nginx if not already installed
 sudo apt-get update
 sudo apt-get -y install nginx
 
+# Create necessary folders
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 
+# Create /data/web_static/releases/test/index.html
 sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html><head></head><body>Do hard things</body></html>" | sudo tee /data/web_static/releases/test/index.html
+# Append Do hard things to /data/web_static/releases/test/index.html
+sudo echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
 
+# Create a symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
+
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
